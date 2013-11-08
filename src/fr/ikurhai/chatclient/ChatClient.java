64a236137d22e99c6d5361e126185407e356ed2c
Ikurhai <ikurhai@hotmail.fr>
2013-11-08 11:30:37 +0100

    Multiple messages sending

--- src/fr/ikurhai/chatclient/ChatClient.java
+++ src/fr/ikurhai/chatclient/ChatClient.java
@@ -3,6 +3,7 @@ package fr.ikurhai.chatclient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -15,64 +16,72 @@ import java.util.Scanner;
  * @version 0.1
  */
 public class ChatClient {
-	
+
 	private Socket socket;
 	private String address;
-    private int port;
-    private Scanner s;
-    
-    
-    /**
-     * Constucteur par défaut
-     */
-    public ChatClient() {
-    	
-    	this.s = new Scanner(System.in);
-    	
-    	System.out.println("- ChatClient -");
+	private int port;
+	private Scanner s;
+
+
+	/**
+	 * Constucteur par défaut
+	 */
+	public ChatClient() {
+
+		this.s = new Scanner(System.in);
+
+		System.out.println("- ChatClient -");
 
 		System.out.print("Enter adress: ");	
 		this.address = s.nextLine();
-		
+
 		System.out.print("Enter port: ");	
 		this.port = s.nextInt();
-    	
-    }
-    
-    
-    /**
-     * Lance le processus du client
-     */
-    public void run() {
-    	
-        BufferedReader in;
-        String message;
-    	
-    	try {
 
-            System.out.println("Connection to " + address + " at " + port + "...");
-            socket = new Socket(address, port);
+	}
 
-            System.out.println("Connected!");
 
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	/**
+	 * Lance le processus du client
+	 */
+	public void run() {
 
-            do {
-                message = in.readLine();
-                System.out.println(message);
-            } while (!message.equals("Disconnected."));
-            
-            System.out.println("Disconnection...");
-            socket.close();
+		PrintWriter out;  
+		BufferedReader in;
+		String message;
 
-            System.out.println("Disconnected!");
+		try {
 
-        } catch (UnknownHostException e) {
-        	e.printStackTrace();
-        } catch (IOException e) {
-        	e.printStackTrace();
-        }
-    	
-    }
-    
+			System.out.println("Connection to " + address + " at " + port + "...");
+			socket = new Socket(address, port);
+
+			System.out.println("Connected!");
+
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream());			
+
+			// MOTD
+			message = in.readLine();
+			System.out.println(message);
+			
+			do {
+				System.out.print("> ");
+				message = s.nextLine();
+				out.println(message);
+				out.flush();
+			} while (!message.equals("/quit"));
+
+			System.out.println("Disconnection...");
+			socket.close();
+
+			System.out.println("Disconnected!");
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
